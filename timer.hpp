978a2cd38924698edb967f0a16f373a45c891a7d
RifeX-LA <timur322132@mail.ru>
2022-08-12 22:28:17 +0300

    timer.hpp: code style fixes

--- timer.hpp
+++ timer.hpp
@@ -18,14 +18,13 @@ namespace tb {
         [[nodiscard]] double elapsed() const {
             using basic_dur = std::chrono::nanoseconds;
 
-            auto elapsed_duration = std::chrono::duration_cast<basic_dur>(clock_t::now() - m_start).count();
-            return static_cast<double>(elapsed_duration) * basic_dur::period::num * dur::period::den
-                   / basic_dur::period::den / dur::period::num;
+            auto basic_elapsed = std::chrono::duration<double, basic_dur::period>(clock_t::now() - m_start).count();
+            return basic_elapsed * basic_dur::period::num * dur::period::den / basic_dur::period::den / dur::period::num;
         }
 
         // returns callable's execution time
-        template <typename Callable, typename ... Args>
-        [[nodiscard]] static double duration(Callable&& callable, Args&& ... args) {
+        template <typename Callable, typename... Args>
+        [[nodiscard]] static double duration(Callable&& callable, Args&&... args) {
             tb::timer<dur> t;
             std::invoke(std::forward<Callable>(callable), std::forward<Args>(args)...);
             return t.elapsed();
@@ -37,7 +36,7 @@ namespace tb {
         }
 
     private:
-        std::chrono::time_point<clock_t> m_start;
+        clock_t::time_point m_start;
     };
 
 }
