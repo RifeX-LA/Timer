560ae05fbdaa3516e42570d1fb52fe10325eb27f
RifeX-LA <timur322132@mail.ru>
2022-12-07 20:20:46 +0300

    udpate timer's signature; add new methods

--- flow/timer.hpp
+++ flow/timer.hpp
@@ -5,19 +5,27 @@
 
 namespace flow {
 
-    template <typename Duration = std::chrono::seconds>
+    /**
+     * @brief std::chrono based timer
+     * @tparam Rep - an arithmetic type representing the number of ticks
+     * @tparam Period - <b>std::ratio</b> representing the tick period
+     */
+    template <typename Rep = double, typename Period = std::ratio<1>>
     class timer {
     public:
+        using rep = Rep;
+        using period = Period;
         using clock_t = std::chrono::steady_clock;
-        using dur = Duration;
 
         timer() : m_start(clock_t::now()) {}
 
         /**
-         * @brief returns elapsed time (default in seconds)
+         * @brief returns elapsed time
          */
-        [[nodiscard]] double elapsed() const {
-            return std::chrono::duration<double, typename dur::period>(clock_t::now() - m_start).count();
+        [[nodiscard]] rep elapsed() const {
+            using to_dur = std::chrono::duration<rep, period>;
+
+            return std::chrono::duration_cast<to_dur>(clock_t::now() - m_start).count();
         }
 
         /**
@@ -26,10 +34,26 @@ namespace flow {
          * @param args arguments to pass to <b>callable</b>
          */
         template <typename Callable, typename... Args>
-        static double duration(Callable&& callable, Args&&... args) {
-            flow::timer<dur> t;
+        static rep duration(Callable&& callable, Args&&... args) {
+            flow::timer<rep, period> timer;
             std::invoke(std::forward<Callable>(callable), std::forward<Args>(args)...);
-            return t.elapsed();
+            return timer.elapsed();
+        }
+
+        /**
+         * @brief returns pair contains callable's execution time and callables' invoke result
+         * @param callable any object, that can be invoked with <b>args</b>
+         * @param args arguments to pass to <b>callable</b>
+         */
+        template <typename Callable, typename... Args>
+        static std::pair<rep, std::invoke_result_t<Callable, Args...>> duration_r(Callable&& callable, Args&&... args) {
+            static_assert(!std::is_void_v<std::invoke_result_t<Callable, Args...>>, "Callable's invoke result must not be void");
+
+            flow::timer<rep, period> timer;
+            auto&& result = std::invoke(std::forward<Callable>(callable), std::forward<Args>(args)...);
+            auto elapsed_time = timer.elapsed();
+
+            return {elapsed_time, std::forward<decltype(result)>(result)};
         }
 
         /**
@@ -39,8 +63,15 @@ namespace flow {
             m_start = clock_t::now();
         }
 
+        /**
+         * @brief returns the time point, from which the timer started counting
+         */
+        clock_t::time_point started_in() noexcept {
+            return m_start;
+        }
+
     private:
         clock_t::time_point m_start;
     };
 
-}
+} //namespace flow
