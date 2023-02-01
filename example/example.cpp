b7e8381503977f7bc3cae4d99a30b82bcd96ae62
RifeX-LA <timur322132@mail.ru>
2023-02-01 16:25:36 +0300

    add avg time functions; update example

--- example/example.cpp
+++ example/example.cpp
@@ -17,16 +17,19 @@ void sort_vector(std::vector<int>& v) {
 int main() {
     flow::timer timer;
     std::vector<int> empty_vector;
-    std::cout << "Create empty vector elapsed: " << timer.elapsed() << " sec\n";
+    std::cout << "Create empty vector time: " << timer.elapsed() << " sec\n";
 
     auto [elapsed_time, vector] = flow::timer<>::elapsed_r(create_vector, 100'000'000);
-    std::cout << "Create vector with 100'000'000 elements elapsed: " << elapsed_time << " sec\n";
+    std::cout << "Create vector with 100'000'000 elements time: " << elapsed_time << " sec\n";
+
+    auto avg_creation_time = flow::timer<>::avg(10, create_vector, 100'000'000);
+    std::cout << "Create vector with 100'000'000 elements average time (10 iterations): " << avg_creation_time << " sec\n";
 
     timer.reset();
     fill_vector(vector);
-    std::cout << "Fill vector elapsed: " << timer.elapsed() << " sec\n";
+    std::cout << "Fill vector time: " << timer.elapsed() << " sec\n";
 
-    std::cout << "Sort vector elapsed: " << flow::timer<int, std::milli>::elapsed(sort_vector, vector) << " ms";
+    std::cout << "Sort vector time: " << flow::timer<int, std::milli>::elapsed(sort_vector, vector) << " ms";
 
     return 0;
 }
